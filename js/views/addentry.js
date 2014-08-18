0d03f6fbb293e7998ce0b5b23794ec657e262c69
Martynas <martynas.bardauskas@gmail.com>
2014-08-18 22:41:26 +0300

    moved addEntry method to the correct view

--- js/views/addentry.js
+++ js/views/addentry.js
@@ -1,8 +1,39 @@
-define(['jquery', 'underscore', 'backbone', 'js/models/entry'], function($, _, Backbone, EntryModel) {
+define([
+	'jquery',
+	'underscore',
+	'backbone',
+	'js/collections/entries',
+	'js/models/entry'], function($, _, Backbone, EntriesCollection, EntryModel) {
 	return Backbone.View.extend({
 		template: _.template( $('#entryAddTemplate').html() ),
 
+		events: {
+			'click #addEntry':'addEntry'
+		},
+
+		addEntry: function(e) {
+			e.preventDefault();
+
+			var self = this;
+			var formData = {};
+
+			$(this.el).find(':input').each(function(i, el) {
+				if( $(el).val() != '' ) {
+					var id = self.parseID(el.id, 'entry-');
+					formData[id] = $(el).val();
+				}
+			});
+
+			console.log(formData);
+			this.collection.create(formData);
+		},
+
+		parseID: function(id, removable) {
+			return id.replace(removable, '');
+		},
+
 		initialize: function() {
+			this.collection = new EntriesCollection();
 			this.render();
 		},
 
--- js/views/entries.js
+++ js/views/entries.js
@@ -6,29 +6,6 @@ define([
 	'js/views/entry'
 	], function($, _, Backbone, EntriesCollection, EntryView) {
 	return Backbone.View.extend({
-		events: {
-			'click #addEntry':'addEntry'
-		},
-
-		addEntry: function(e) {
-			e.preventDefault();
-
-			var formData = {};
-
-			function parseID(id, removable) {
-				return id.replace(removable, '');
-			}
-
-			$('#entry-form').find(':input').each(function(i, el) {
-				if( $(el).val() != '' ) {
-					var id = parseID(el.id, 'entry-');
-					formData[id] = $(el).val();
-				}
-			});
-
-			this.collection.create(formData);
-		},
-
 		initialize: function() {
 			// @TODO: make User as a singleton
 			/*var user = new app.User;
