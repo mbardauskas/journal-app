6315202a17b69e26a9a30d5e7a99422489403334
Martynas <martynas.bardauskas@gmail.com>
2014-08-26 22:14:16 +0300

    moved route related events to router

--- js/appEvents.js
+++ js/appEvents.js
@@ -6,12 +6,5 @@ define([
 	var appEvents = {};
 	_.extend(appEvents, Backbone.Events);
 
-	appEvents.on("userLoggedIn", function() {
-		Backbone.history.navigate('', {trigger: true});
-	});
-	appEvents.on("userLoggedOut", function() {
-		Backbone.history.navigate('loggedout', {trigger: true});
-	});
-
 	return appEvents;
 });
--- js/appRouter.js
+++ js/appRouter.js
@@ -2,19 +2,36 @@ define([
 	'jquery',
 	'underscore',
 	'backbone',
+	'js/appEvents',
 	'js/models/user',
-	'js/views/view-controller'], function($, _, Backbone, User, ViewController) {
+	'js/views/view-controller'], function($, _, Backbone, appEvents, User, ViewController) {
 		return Backbone.Router.extend({
 			routes: {
+				// user is not navigated if the fragment is the same,
+				// thus there are 3 routes to index.
 				'': 'index',
+				'index': 'index',
 				'loggedout': 'index',
+				// login/logout
 				'login': 'login',
 				'logout': 'logout',
+				// other
 				'entries': 'entries',
 				'entries/add': 'addEntry',
 				'entries/:id/edit': 'editEntry'
 			},
 			initialize: function() {
+				appEvents.on("userLoggedIn", function() {
+					if(Backbone.history.fragment === '') {
+						Backbone.history.navigate('index', {trigger: true});
+					} else {
+						Backbone.history.navigate('', {trigger: true});
+					}
+				});
+				appEvents.on("userLoggedOut", function() {
+					Backbone.history.navigate('loggedout', {trigger: true});
+				});
+
 				this.viewController = new ViewController();
 				this.viewController.initPage();
 			},
